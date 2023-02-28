4839abf3c11beb46f58106cd466648ae89c07b13
JB Tellez <jb.tellez@gmail.com>
2023-02-28 09:36:28 -0800

    starting point of demo 12

---
+++ class-12/demo/back-end/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+const Cat = require('./models/cat');
+
+const PORT = process.env.PORT;
+
+
+
+
+const app = express();
+app.use(cors());
+
+mongoose.connect(process.env.DATABASE_URL);
+
+app.get('/cats', async (request, response) => {
+
+  const filterQuery = {};
+
+  if (request.query.location) {
+    filterQuery.location = request.query.location;
+  }
+
+  const cats = await Cat.find(filterQuery);
+
+  response.send(cats);
+});
+
+
+
+
+app.listen(PORT, () => console.log('listening on PORT:', PORT));
