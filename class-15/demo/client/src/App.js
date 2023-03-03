abf8258efe0057dada2930b254ff42ee680963f4
JB Tellez <jb.tellez@gmail.com>
2023-03-03 00:32:13 -0800

    updates class 13 demo dependencies

--- class-15/demo/client/src/App.js
+++ class-15/demo/client/src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { withAuth0 } from '@auth0/auth0-react';
 import AuthButtons from './Auth/AuthButtons';
-import Login from './Auth/Login';
-import Logout from './Auth/Logout';
 import Profile from './Profile';
 import Content from './Content';
 
@@ -14,13 +12,6 @@ class App extends React.Component {
           Login or Logout with one component <AuthButtons />
         </div>
 
-        <div>
-          Login with a standalone component <Login />
-        </div>
-
-        <div>
-          Logout with a standalone component <Logout />
-        </div>
 
         {this.props.auth0.isAuthenticated &&
           <>
